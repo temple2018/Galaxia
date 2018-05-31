2ecb391ad56f79834250c0c83324ae3e332861b8
meawoppl <meawoppl@gmail.com>
2018-05-30 20:56:29 -0700

    Add header load from resources

--- src/main/java/org/templegalaxia/model/Petal.java
+++ src/main/java/org/templegalaxia/model/Petal.java
@@ -4,36 +4,64 @@ import heronarts.lx.model.LXAbstractFixture;
 import heronarts.lx.model.LXModel;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.transform.LXTransform;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import processing.core.PApplet;
-import processing.data.Table;
-import processing.data.TableRow;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Petal extends LXModel {
 
-    public Petal(Table pixelLocations, LXTransform transform) {
-        super(new Fixture(pixelLocations, transform));
+    public Petal(LXTransform transform) {
+        super(new Fixture(transform));
     }
 
-    public static Petal loadFromCsv(PApplet applet, LXTransform transform, String fileName) {
-        Table pixels = applet.loadTable(fileName, "header");
+    private static List<float[]> xyzs = new ArrayList<>();
 
-        return new Petal(pixels, transform);
+    private static void encacheCSV() {
+        // Already cached/parsed
+        if(xyzs.size() != 0){
+            return;
+        }
 
+        CSVParser parser;
+        try {
+            parser = new CSVParser(
+                new BufferedReader(new InputStreamReader(Petal.class.getResourceAsStream("points.csv"))),
+                CSVFormat.EXCEL.withHeader());
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+
+        // Read in all the xyzs
+        // NOTE(G3): There is a coordinate systems mismatched between rhino & Processing
+        // and this foolery ensures that the temple is upright in processing.
+        for (CSVRecord csvRecord : parser) {
+            xyzs.add(new float[]{
+                Float.parseFloat(csvRecord.get("y")),
+                Float.parseFloat(csvRecord.get("z")),
+                Float.parseFloat(csvRecord.get("x"))
+            });
+        }
+
+        System.out.println(String.format("Loaded %d points.", xyzs.size()));
+    }
+
+    public static Petal loadFromCsv(PApplet applet, LXTransform transform) {
+        encacheCSV();
+        return new Petal(transform);
     }
 
     private static class Fixture extends LXAbstractFixture {
-
-        Fixture(Table pixelLocations, LXTransform transform) {
-            for (TableRow row : pixelLocations.rows()) {
-                // NOTE(G3): There is a coordinate systems mismatched between rhino & Processing
-                // and this foolery ensures that the temple is upright in processing.
-                float x = row.getFloat("y");
-                float y = row.getFloat("z");
-                float z = row.getFloat("x");
-
+        Fixture(LXTransform transform) {
+            for (float[] xyz : xyzs) {
                 transform.push();
-                transform.translate(x, y, z);
 
+                transform.translate(xyz[0], xyz[1], xyz[2]);
                 addPoint(new LXPoint(transform));
 
                 transform.pop();
