8618d4ab93cac8c23226e653aaac6b7eacbe0d6f
geethree <gerald@3scan.com>
2018-05-31 20:42:18 -0700

    Add petal interator and matty's first pattern.

--- src/main/java/org/templegalaxia/patterns/TemplePattern.java
+++ src/main/java/org/templegalaxia/patterns/TemplePattern.java
@@ -2,6 +2,7 @@ package org.templegalaxia.patterns;
 
 import heronarts.lx.LX;
 import heronarts.lx.LXPattern;
+import org.templegalaxia.model.Petal;
 import org.templegalaxia.model.Temple;
 
 
@@ -11,5 +12,17 @@ public abstract class TemplePattern extends LXPattern {
     public TemplePattern(LX lx) {
         super(lx);
         model = (Temple)lx.model;
+    }
+
+    public int pointIndexToPetalIndex(int pointIndex) {
+        return pointIndex % Petal.NUM_PIXELS;
+    }
+
+    public int pointIndexToPetalNumber(int pointIndex) {
+        return pointIndex / Petal.NUM_PIXELS;
+    }
+
+    public int petalIndexToPointIndex(int petalIndex, int petalNumber) {
+        return (petalNumber * Petal.NUM_PIXELS) + petalIndex;
     }
 }
