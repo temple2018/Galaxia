29839edc8d6fc4e237bbd61d249095acb9812475
meawoppl <meawoppl@gmail.com>
2018-06-16 14:06:34 -0700

    Testable?

--- src/main/java/org/templegalaxia/DemuxArtNetDatagram.java
+++ src/main/java/org/templegalaxia/DemuxArtNetDatagram.java
@@ -1,5 +1,7 @@
 package org.templegalaxia;
 
+import heronarts.lx.LXUtils;
+import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXFixture;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.output.LXDatagram;
@@ -13,86 +15,94 @@ public class DemuxArtNetDatagram extends LXDatagram {
         private static final int ARTNET_PORT = 6454;
         private static final int SEQUENCE_INDEX = 12;
         private final int[] pointIndices;
-        private boolean sequenceEnabled;
         private byte sequence;
 
         public DemuxArtNetDatagram(int[] var1) {
-            this((int[])var1, 0);
+            this((int[])var1, DEFAULT_UNIVERSE);
         }
 
-        public DemuxArtNetDatagram(LXFixture var1, int var2) {
-            this(fixtureToIndices(var1), var2);
+        public DemuxArtNetDatagram(LXFixture fixture, int var2) {
+            this(fixtureToIndices(fixture), var2);
         }
 
-        public DemuxArtNetDatagram(int[] var1, int var2) {
-            this(var1, 3 * var1.length, var2);
+        public DemuxArtNetDatagram(int[] indices, int universe) {
+            this(indices, 3 * indices.length, universe);
         }
 
-        public DemuxArtNetDatagram(LXFixture var1, int var2, int var3) {
-            this(fixtureToIndices(var1), var2, var3);
+        public DemuxArtNetDatagram(LXFixture var1, int dataLength, int universe) {
+            this(fixtureToIndices(var1), dataLength, universe);
         }
 
         // Stolen from LXOutput
         static int[] fixtureToIndices(LXFixture var0) {
-            List var1 = var0.getPoints();
-            int[] var2 = new int[var1.size()];
+            List points = var0.getPoints();
+            int[] indices = new int[points.size()];
             int var3 = 0;
 
+            // This loop is unrolled weirdly, but I believe returns a int[] of
+            // the indices represented by the fixture
             LXPoint var5;
-            for(Iterator var4 = var1.iterator(); var4.hasNext(); var2[var3++] = var5.index) {
-                var5 = (LXPoint)var4.next();
+            for(Iterator pointsIter = points.iterator(); pointsIter.hasNext(); indices[var3++] = var5.index) {
+                var5 = (LXPoint) pointsIter.next();
             }
 
-            return var2;
+            return indices;
+        }
+
+        public DemuxArtNetDatagram(int[] indices, int dataLength, int universe) {
+            if(indices.length % 3 != 0){
+                throw new AssertionError("Indicies has the wrong length:" + indices.length);
+            }
+            
+            super(ARTNET_HEADER_LENGTH + indices.length;
+            this.sequence = 1;
+            this.pointIndices = indices;
+
+            this.setPort(ARTNET_PORT);
+            this.buffer[0] = 65;
+            this.buffer[1] = 114;
+            this.buffer[2] = 116;
+            this.buffer[3] = 45;
+            this.buffer[4] = 78;
+            this.buffer[5] = 101;
+            this.buffer[6] = 116;
+            this.buffer[7] = 0;
+            this.buffer[8] = 0;
+            this.buffer[9] = 80;
+            this.buffer[10] = 0;
+            this.buffer[11] = 14;
+            this.buffer[12] = 0;
+            this.buffer[13] = 0;
+            this.buffer[14] = (byte)(universe & 255);
+            this.buffer[15] = (byte)(universe >>> 8 & 255);
+            this.buffer[16] = (byte)(dataLength >>> 8 & 255);
+            this.buffer[17] = (byte)(dataLength & 255);
+
+            for(int var4 = ARTNET_HEADER_LENGTH; var4 < this.buffer.length; ++var4) {
+                this.buffer[var4] = 0;
+            }
+        }
+
+        public byte luminance(int rgb){
+            int r = LXColor.red(rgb);
+            int g = LXColor.green(rgb);
+            int b = LXColor.blue(rgb);
+
+            return (byte) Math.round((r + g + b)/ 3.0);
         }
 
 
-        public DemuxArtNetDatagram(int[] var1, int var2, int var3) {
-                super(18 + var2 + var2 % 2);
-                this.sequenceEnabled = false;
-                this.sequence = 1;
-                this.pointIndices = var1;
-                this.setPort(6454);
-                this.buffer[0] = 65;
-                this.buffer[1] = 114;
-                this.buffer[2] = 116;
-                this.buffer[3] = 45;
-                this.buffer[4] = 78;
-                this.buffer[5] = 101;
-                this.buffer[6] = 116;
-                this.buffer[7] = 0;
-                this.buffer[8] = 0;
-                this.buffer[9] = 80;
-                this.buffer[10] = 0;
-                this.buffer[11] = 14;
-                this.buffer[12] = 0;
-                this.buffer[13] = 0;
-                this.buffer[14] = (byte)(var3 & 255);
-                this.buffer[15] = (byte)(var3 >>> 8 & 255);
-                this.buffer[16] = (byte)(var2 >>> 8 & 255);
-                this.buffer[17] = (byte)(var2 & 255);
+        public void onSend(int[] colors) {
+            // colors[] and this.pointIndices are 1:1 shape wise
+            // Here we map colors[0] -> "R", colors[1] -> "G", colors[2] -> "B"
 
-                for(int var4 = 18; var4 < this.buffer.length; ++var4) {
-                    this.buffer[var4] = 0;
-                }
-
+            int offset = 0;
+            for( int ptIndex : this.pointIndices ){
+                this.buffer[ARTNET_HEADER_LENGTH + offset] = luminance(colors[ptIndex]);
+                ++offset;
             }
-
-        public heronarts.lx.output.ArtNetDatagram setSequenceEnabled(boolean var1) {
-            this.sequenceEnabled = var1;
-            return this;
         }
 
-        public void onSend(int[] var1) {
-            this.copyPoints(var1, this.pointIndices, 18);
-            if (this.sequenceEnabled) {
-                if (++this.sequence == 0) {
-                    ++this.sequence;
-                }
 
-                this.buffer[12] = this.sequence;
-            }
 
-        }
-    }
 }
