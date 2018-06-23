0a1e68ff3616bcbb994ee5dd182dd2324891e390
meawoppl <meawoppl@gmail.com>
2018-06-22 17:47:37 -0700

    Spotless

--- src/main/java/org/templegalaxia/patterns/TemplePattern.java
+++ src/main/java/org/templegalaxia/patterns/TemplePattern.java
@@ -5,24 +5,23 @@ import heronarts.lx.LXPattern;
 import org.templegalaxia.model.Petal;
 import org.templegalaxia.model.Temple;
 
-
 public abstract class TemplePattern extends LXPattern {
-    protected final Temple model;
+  protected final Temple model;
 
-    public TemplePattern(LX lx) {
-        super(lx);
-        model = (Temple)lx.model;
-    }
+  public TemplePattern(LX lx) {
+    super(lx);
+    model = (Temple) lx.model;
+  }
 
-    public int pointIndexToPetalIndex(int pointIndex) {
-        return pointIndex % Petal.NUM_PIXELS;
-    }
+  public int pointIndexToPetalIndex(int pointIndex) {
+    return pointIndex % Petal.NUM_PIXELS;
+  }
 
-    public int pointIndexToPetalNumber(int pointIndex) {
-        return pointIndex / Petal.NUM_PIXELS;
-    }
+  public int pointIndexToPetalNumber(int pointIndex) {
+    return pointIndex / Petal.NUM_PIXELS;
+  }
 
-    public int petalIndexToPointIndex(int petalIndex, int petalNumber) {
-        return (petalNumber * Petal.NUM_PIXELS) + petalIndex;
-    }
+  public int petalIndexToPointIndex(int petalIndex, int petalNumber) {
+    return (petalNumber * Petal.NUM_PIXELS) + petalIndex;
+  }
 }
--- src/main/java/org/templegalaxia/patterns/matty/Teleport.java
+++ src/main/java/org/templegalaxia/patterns/matty/Teleport.java
@@ -11,51 +11,47 @@ import processing.core.PApplet;
 
 @LXCategory("MattyG")
 public class Teleport extends TemplePattern {
-    BoundedParameter speed = new BoundedParameter(
-            "speed",
-            5000, 10000, 250);
+  BoundedParameter speed = new BoundedParameter("speed", 5000, 10000, 250);
 
-    BoundedParameter bandWidth = new BoundedParameter(
-            "bandwidth",
-            (model.yMax - model.yMin) / 10,
-            (model.yMax - model.yMin) / 100,
-            (model.yMax - model.yMin) / 2);
+  BoundedParameter bandWidth =
+      new BoundedParameter(
+          "bandwidth",
+          (model.yMax - model.yMin) / 10,
+          (model.yMax - model.yMin) / 100,
+          (model.yMax - model.yMin) / 2);
 
-    BoundedParameter acceleration = new BoundedParameter(
-            "Accel",
-            1, 0.5, 5);
+  BoundedParameter acceleration = new BoundedParameter("Accel", 1, 0.5, 5);
 
-    SawLFO saw = new SawLFO(model.yMin, model.yMax, speed);
+  SawLFO saw = new SawLFO(model.yMin, model.yMax, speed);
 
-    public Teleport(LX lx) {
-        super(lx);
-        addModulator(saw).trigger();
-        addParameter(acceleration);
-        addParameter(bandWidth);
-        addParameter(speed);
+  public Teleport(LX lx) {
+    super(lx);
+    addModulator(saw).trigger();
+    addParameter(acceleration);
+    addParameter(bandWidth);
+    addParameter(speed);
+  }
+
+  public void run(double deltaMs) {
+    float currentY = saw.getValuef();
+    float halfBandwidth = (bandWidth.getValuef() / 2);
+    currentY =
+        PApplet.map(
+            currentY,
+            model.yMin,
+            model.yMax,
+            model.yMin - halfBandwidth,
+            model.yMax + halfBandwidth);
+
+    for (LXPoint p : model.points) {
+      boolean aboveMin = (p.y - halfBandwidth) < currentY;
+      boolean belowMax = (p.y + halfBandwidth) > currentY;
+
+      if (aboveMin && belowMax) {
+        colors[p.index] = LXColor.WHITE;
+      } else {
+        colors[p.index] = LXColor.BLACK;
+      }
     }
-
-    public void run(double deltaMs) {
-        float currentY = saw.getValuef();
-        float halfBandwidth = (bandWidth.getValuef() / 2);
-        currentY = PApplet.map(
-                currentY,
-                model.yMin,
-                model.yMax,
-                model.yMin - halfBandwidth,
-                model.yMax + halfBandwidth
-        );
-
-        for(LXPoint p : model.points) {
-            boolean aboveMin = (p.y - halfBandwidth) < currentY;
-            boolean belowMax = (p.y + halfBandwidth) > currentY;
-
-            if(aboveMin && belowMax){
-                colors[p.index] = LXColor.WHITE;
-            } else {
-                colors[p.index] = LXColor.BLACK;
-            }
-
-        }
-    }
+  }
 }
