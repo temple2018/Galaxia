af320f11fd722ab662b7f01b98a08d759d6c3d34
meawoppl <meawoppl@gmail.com>
2018-07-14 16:04:23 -0700

    Nasty datagram

---
+++ src/main/java/org/templegalaxia/SemiDemusAtNetDatagram.java
@@ -0,0 +1,65 @@
+package org.templegalaxia;
+
+import heronarts.lx.color.LXColor;
+import heronarts.lx.model.LXFixture;
+import heronarts.lx.output.ArtNetDatagram;
+
+public class SemiDemusAtNetDatagram extends ArtNetDatagram {
+    private static final int ARTNET_HEADER_LENGTH = 18;
+    private final int[] demuxed;
+    private final int[] muxed;
+
+    public static SemiDemusAtNetDatagram fromFixture(LXFixture demuxed, LXFixture muxed, int universe) {
+        return new SemiDemusAtNetDatagram(
+                LXFixture.Utils.getIndices(demuxed),
+                LXFixture.Utils.getIndices(muxed),
+                universe);
+    }
+
+    private int[] nastyArrayAdd(int[] a, int[] b){
+        int[] r = new int[a.length + b.length];
+        System.arraycopy(a, 0, r, 0, a.length);
+        System.arraycopy(b, 0, r, a.length, b.length);
+        return r;
+    }
+
+    private DemuxArtNetDatagram(int[] demuxed, int[] muxed, int universe) {
+        super(
+            this.nastyArrayAdd(demuxed, muxed),
+            demuxed.length + muxed.length,
+            universe);
+        this.demuxed = demuxed;
+        this.muxed = muxed;
+    }
+
+    public byte luminance(int rgb) {
+        int r = (rgb >> LXColor.R_SHIFT) & 0xFF;
+        int g = (rgb >> LXColor.G_SHIFT) & 0xFF;
+        int b = (rgb) & LXColor.B_MASK;
+
+        return (byte) ((r + g + b) / 3);
+    }
+
+    @Override
+    public void onSend(int[] colors) {
+        // colors[] and this.demuxed are 1:1 shape wise
+        // Here we map colors[0] -> "R", colors[1] -> "G", colors[2] -> "B"
+
+        int offset = ARTNET_HEADER_LENGTH;
+        for (int ptIndex : this.demuxed) {
+            this.buffer[offset] = luminance(colors[ptIndex]);
+            ++offset;
+        }
+
+        for (int ptIndex : this.muxed) {
+            this.buffer[offset] = LXColor.blue(colors[ptIndex]);
+            ++offset;
+
+            this.buffer[offset] = LXColor.green(colors[ptIndex]);
+            ++offset;
+
+            this.buffer[offset] = LXColor.red(colors[ptIndex]);
+            ++offset;
+        }
+    }
+}
