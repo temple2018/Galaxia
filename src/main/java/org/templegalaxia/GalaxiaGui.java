d796b4c3e1a473e2bbbd9a380d3d8f27e8cc8572
Alan Persak <>
2018-08-04 14:42:03 -0700

    created DebugPatern to print model stats. Improved and cleanup to WavePattern. waves are now smooth and randomized.

--- src/main/java/org/templegalaxia/GalaxiaGui.java
+++ src/main/java/org/templegalaxia/GalaxiaGui.java
@@ -4,6 +4,7 @@ import heronarts.lx.model.LXModel;
 import heronarts.lx.studio.LXStudio;
 import org.templegalaxia.configuration.Outputs;
 import org.templegalaxia.model.Temple;
+import org.templegalaxia.patterns.alanpersak.DebugPattern;
 import org.templegalaxia.patterns.alanpersak.MessagePattern;
 import org.templegalaxia.patterns.alanpersak.StarPattern;
 import org.templegalaxia.patterns.alanpersak.WavePattern;
@@ -57,9 +58,12 @@ public class GalaxiaGui extends PApplet {
     lx.registerPattern(DebugOrder.class);
     lx.registerPattern(Swirl.class);
     lx.registerPattern(RingTest.class);
+
+    // Alan Persak's patterns:
     lx.registerPattern(MessagePattern.class);
     lx.registerPattern(StarPattern.class);
     lx.registerPattern(WavePattern.class);
+    lx.registerPattern(DebugPattern.class);
   }
 
   public void draw() {
--- src/main/java/org/templegalaxia/patterns/alanpersak/WavePattern.java
+++ src/main/java/org/templegalaxia/patterns/alanpersak/WavePattern.java
@@ -16,17 +16,25 @@ import processing.core.PApplet;
 @LXCategory("Alan Persak")
 public class WavePattern extends TemplePattern {
 
+    private static final double TAU = 2 * Math.PI;
+    private static final double TIME_SCALE = 5;
+    private static final double DECAY = 5000;
+    private static final int MIN_BRIGHTNESS = 10;
+    private static final double RAMP = 2000;
+    private static final double SPEED = 500;
+
     private Random random = new Random();
     private LinkedList<Wave> waves = new LinkedList<Wave>();
     private int numPoints;
     private double[] angles;
     private double[] radii;
+    private double waveTimer;
+    private double nextWaveTime = 0;
 
     public WavePattern(LX lx) {
         super(lx);
-        //waves.add(new Wave(1000, 1000, 1, 0, 100));
 
-        // precompute pixel data
+        // Precompute point data
         numPoints = model.points.length;
         angles = new double[numPoints];
         radii = new double[numPoints];
@@ -37,101 +45,143 @@ public class WavePattern extends TemplePattern {
         }
     }
 
-    double waveTimer = 0;
+    double printTimer = 0;
     int frameCounter = 0;
-    double dir = 0;
 
     public void run(double deltaMs) {
-        deltaMs *= 5;
-        waveTimer += deltaMs / 1000;
         frameCounter++;
-        if(waveTimer > 10){
 
-            //double size = (50 + random.nextInt(151)) / 100.0;
-            //double direction = random.nextInt(360) / 360.0 * 2 * Math.PI;
-            double size = 1;
+        // Print stats every 10 real seconds.
+        printTimer += deltaMs / 1000;
+        if(printTimer > 10){
+            /*double size = 1;
             double direction = dir;
             dir += Math.PI/2;
             if(dir >= 2*Math.PI){
                 dir = 0;
             }
             System.out.println("new wave " + size + " " + direction);
-            waves.add(new Wave(1000, 1000, size, direction, 100));
-
-
+            waves.add(new Wave(MIN_POSITION, 1000, size, direction, 100));*/
 
             System.out.println("\nstats:");
             System.out.println("wave count: " + waves.size());
             System.out.println("frame rate: " + frameCounter / waveTimer);
 
-            waveTimer = 0;
+            printTimer = 0;
             frameCounter = 0;
+        }
+
+        // Create new waves based on scaled time.
+        double scaledDelta = deltaMs * TIME_SCALE;
+        waveTimer += scaledDelta / 1000;
+        if(waveTimer > nextWaveTime){
+            waveTimer = 0;
+            nextWaveTime = randomDouble(random, 5, 10, 100);
+
+            double direction = randomDouble(random, 0, TAU, 360);
+            double size = randomDouble(random, .5, 3, 100);
+            waves.add(new Wave(MIN_POSITION, SPEED, size, direction, 100));
+
+            System.out.println("\nnew wave: " + direction + " " + size);
+            System.out.println("next wave: " + nextWaveTime);
         }
 
         // Turn everything off.
         for (LXPoint p : model.points) {
-            colors[p.index] = LXColor.BLACK;
+            //colors[p.index] = LXColor.BLACK;
+            colors[p.index] = LXColor.hsb(0, 0, MIN_BRIGHTNESS);
         }
 
+        // Animate the waves.
         for(Iterator<Wave> it = waves.iterator(); it.hasNext(); ){
             Wave wave = it.next();
-            wave.run(deltaMs);
+            wave.run(scaledDelta);
             if(wave.done()){
                 it.remove();
             }
         }
     }
 
-    private static double MIN_POSITION = 1;
-
-    // max radius: 28769.12613185006
-    private static final double MAX_POSITION = 60000;
-
-    private double angle(LXPoint point) {
-        return Math.atan2(point.z, point.x) + Math.PI;
-    }
+    private static double MIN_POSITION = 1000; // Minimum measured radius: 3508
+    private static final double MAX_POSITION = 60000;  // Maximum measured radius: 28769
 
     private class Wave {
 
-        double direction; // angle of the waves center.
-        double size;    // angle of the waves edges, going out from center.
-        double speed; // rate that arc radius expands.
-        double position; // arc radius.
-        double intensity;
+        double direction; // Direction of the center of the wave, in radians.
+        double minAngle;
+        double maxAngle;
+        double speed;     // Rate that wave's arc radius expands.
+        double position;  // Current arc radius.
+        double intensity; // Maximum brightness at the leading edge of the wave.
 
         public Wave(double position, double speed, double size, double direction, double intensity) {
             this.position = position;
             this.speed = speed;
-            this.size = size;
+            this.minAngle = clampAngle(direction - size/2);
+            this.maxAngle = clampAngle(direction + size/2);
             this.direction = direction;
             this.intensity = intensity;
         }
 
         public void run(double deltaMs) {
             position += speed * deltaMs / 1000;
-            //System.out.println("pos: " + position);
 
             for (int i=0; i<numPoints; i++){
-                //double angle = angle(point);
-                //double radius = Math.hypot(point.x, point.z);
-                //System.out.println("point " + point.x + " " + point.y + " " + angle + " " + radius);
                 double angle = angles[i];
-                if (angle < direction - size || angle > direction + size){
+                if (!isAngleInRange(angle, minAngle, maxAngle)){
                     continue;
                 }
-                //double radius = Math.hypot(point.x, point.y);
                 double radius = radii[i];
-                if (radius > position) {
+                if (radius > position + RAMP) {
                     continue;
                 }
-                int brightness = (int)(intensity * Math.pow(2, (radius - position)/5000));
-                LXPoint point = model.points[i];
-                colors[model.getPixelIndex(point)] = LXColor.hsb(0, 0, brightness);
+                double brightness = LXColor.b(colors[model.points[i].index]);
+                if (radius > position) {
+                    brightness = (int)linearInterpolate(position + RAMP - radius, 0, brightness, RAMP, intensity);
+
+                } else {
+                    brightness = (int) (intensity * Math.pow(2, (radius - position) / DECAY));
+                    brightness = Math.max(brightness, MIN_BRIGHTNESS);
+                }
+                colors[model.points[i].index] = LXColor.hsb(0, 0, brightness);
             }
         }
 
         public boolean done() {
             return position > MAX_POSITION;
         }
+    }
+
+    // Returns whether an angle is within a range, while accounting for the boundary. All values are expected to be
+    // in range [0, 2PI).
+    private static boolean isAngleInRange(double angle, double min, double max){
+        if (min < max) {
+            return angle >= min && angle <= max;
+        }
+        // else the range wraps around the circle boundary.
+        return angle >= min || angle < max;
+    }
+
+    // Limits an angle in radians to range [0, 2PI).
+    private static double clampAngle(double angle) {
+        while(angle < 0){
+            angle += TAU;
+        }
+        while (angle >= TAU){
+            angle -= TAU;
+        }
+        return angle;
+    }
+
+    private static double angle(LXPoint point) {
+        return Math.atan2(point.z, point.x) + Math.PI;
+    }
+
+    private static double randomDouble(Random r, double min, double max, int precision){
+        return r.nextInt(precision) * (max - min) / precision + min;
+    }
+
+    private static double linearInterpolate(double x, double x0, double y0, double x1, double y1){
+        return (y0*(x1-x)+y1*(x-x0))/(x1-x0);
     }
 }
