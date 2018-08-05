25ab63ddedaf4b97699c6d62579442eb309ab7e8
Gerald Spencer <gerald@3scan.com>
2018-08-04 17:13:07 -0700

    Spotless

--- src/main/java/org/templegalaxia/patterns/mcslee/Clouds.java
+++ src/main/java/org/templegalaxia/patterns/mcslee/Clouds.java
@@ -8,28 +8,29 @@ import heronarts.lx.LXUtils;
 import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.HistoryBuffer;
-import heronarts.lx.modulator.SinLFO;
 import heronarts.lx.parameter.CompoundParameter;
 import org.templegalaxia.model.Ring;
 import org.templegalaxia.patterns.TemplePattern;
 
 public class Clouds extends TemplePattern {
 
-  public final CompoundParameter speed = new CompoundParameter("Range", 5, 1, 10)
-          .setDescription("Range of the cloud motion");
+  public final CompoundParameter speed =
+      new CompoundParameter("Range", 5, 1, 10).setDescription("Range of the cloud motion");
 
-  public final CompoundParameter value = new CompoundParameter("Value", .5)
+  public final CompoundParameter value =
+      new CompoundParameter("Value", .5)
           .setDescription("Value to be modulated to generate randomness in the rising cloud");
 
-  public final CompoundParameter contrast = new CompoundParameter("Constrast", 1, 1, 4)
-          .setDescription("Amount to increase contrast by");
+  public final CompoundParameter contrast =
+      new CompoundParameter("Constrast", 1, 1, 4).setDescription("Amount to increase contrast by");
 
-  private final HistoryBuffer history = new HistoryBuffer("History", 4096) {
-    @Override
-    protected double getFrameValue() {
-      return value.getValue();
-    }
-  };
+  private final HistoryBuffer history =
+      new HistoryBuffer("History", 4096) {
+        @Override
+        protected double getFrameValue() {
+          return value.getValue();
+        }
+      };
 
   public Clouds(LX lx) {
     super(lx);
