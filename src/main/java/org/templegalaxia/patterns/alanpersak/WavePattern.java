72815121bbfb170237896dc6106f1bdac4a906f1
Alan Persak <>
2018-08-05 15:02:17 -0700

    finished cleaning up wave pattern for review. all parameters are exposed.

--- src/main/java/org/templegalaxia/patterns/alanpersak/WavePattern.java
+++ src/main/java/org/templegalaxia/patterns/alanpersak/WavePattern.java
@@ -17,27 +17,50 @@ import processing.core.PApplet;
 public class WavePattern extends TemplePattern {
 
     private static final double TAU = 2 * Math.PI;
-    private static final double TIME_SCALE = 5;
-    private static final double DECAY = 10000;
-    private static final int MIN_BRIGHTNESS = 5;
-    private static final double SPEED = 500;
-    private static final double FRONT_FADE = 1500;
-    private static final double SIDE_FADE = .2;
-    private static final double MIN_WAVE_DIRECTION_OFFSET = Math.PI/2;
+    private static double MIN_POSITION = 0; // Initial wave radius. Minimum pixel radius is 3508.
+    private static final double MAX_POSITION = 100000; // Maximum wave radius before it is removed from the simulation. Maximum pixel radius is 28769.
+
+    private BoundedParameter timeScaleParam = new BoundedParameter("Time Scale", 1, .1, 10).setDescription("Scales the speed of the wave simulation.");
+    private BoundedParameter decayParam = new BoundedParameter("Decay Rate", 10000, 1000, 100000).setDescription("The rate of exponential decay behind the wave front, as half-life distance. E.g. at 1000, the brightness will halve every 1000 distance units from the wave front.");;
+    private BoundedParameter fadeDistanceParam = new BoundedParameter("Fade Distance", 1500, 0,10000).setDescription("The distance from the wave front over which the wave fades in. The brightness is linearly interpolated from the current brightness to the wave's brightness.");
+    private BoundedParameter fadeAngleParam = new BoundedParameter("Fade Angle", .2, 0, .5).setDescription("The angle over which the brightness fades at the edge of the wave.");
+    private BoundedParameter baseBrightnessParam = new BoundedParameter("Base Brightness", 5, 0, 100).setDescription("The background brightness.");
+    private BoundedParameter waveBrightnessParam = new BoundedParameter("Wave Brightness", 100, 0, 200).setDescription("The brightness at the peak of the wave front. Going above 100 will create a flat top where the brightness is saturated.");
+    private BoundedParameter waveSpeedParam = new BoundedParameter("Wave Speed", 500, 100, 10000).setDescription("The rate at which the wave arc radius expands.");
+    private BoundedParameter minWaveAngleParam = new BoundedParameter("Minimum Wave Angle", 1, 0, TAU).setDescription("The minimum angle of the randomized wave size.");
+    private BoundedParameter maxWaveAngleParam = new BoundedParameter("Maximum Wave Angle", 1, 0, TAU).setDescription("The maximum angle of the randomized wave size.");
+    private BoundedParameter minWaveTimeParam = new BoundedParameter("Minimum Wave Period", 20, 1, 300).setDescription("The minimum time of the randomized period between waves, in seconds.");
+    private BoundedParameter maxWaveTimeParam = new BoundedParameter("Maximum Wave Period", 30, 1, 300).setDescription("The maximum time of the randomized period between waves, in seconds.");
+    private BoundedParameter offsetParam = new BoundedParameter("Direction Offset", Math.PI/2, 0, Math.PI).setDescription("The minimum angle between successive wave directions, which allows for a more balanced distribution.");
 
     private Random random = new Random();
     private LinkedList<Wave> waves = new LinkedList<Wave>();
-    private int numPoints;
-    private double[] angles;
-    private double[] radii;
-    private double waveTimer;
-    private double nextWaveTime = 0;
-    private double lastWaveDirection = 0;
+    private int numPoints; // Number of points/pixels.
+    private double[] angles; // Angles of each point from the origin.
+    private double[] radii; // Radii of each point from the origin.
+    private double waveTimer; // Timer which counts up to next wave.
+    private double nextWaveTime = 0; // Time of next wave.
+    private double lastWaveDirection = 0; // Direction of last wave, used to offset the directions of successive waves.
+    double statTimer = 0; // Timer which counts for printing stats.
+    int frameCounter = 0; // Number of animation frames.
 
     public WavePattern(LX lx) {
         super(lx);
 
-        // Precompute point data
+        addParameter(timeScaleParam);
+        addParameter(decayParam);
+        addParameter(fadeDistanceParam);
+        addParameter(fadeAngleParam);
+        addParameter(baseBrightnessParam);
+        addParameter(waveBrightnessParam);
+        addParameter(waveSpeedParam);
+        addParameter(minWaveAngleParam);
+        addParameter(maxWaveAngleParam);
+        addParameter(minWaveTimeParam);
+        addParameter(maxWaveTimeParam);
+        addParameter(offsetParam);
+
+        // Precompute point data in lookup tables for efficiency.
         numPoints = model.points.length;
         angles = new double[numPoints];
         radii = new double[numPoints];
@@ -47,9 +70,6 @@ public class WavePattern extends TemplePattern {
             radii[i] = Math.hypot(point.x, point.z);
         }
     }
-
-    double statTimer = 0;
-    int frameCounter = 0;
 
     public void run(double deltaMs) {
 
@@ -65,30 +85,36 @@ public class WavePattern extends TemplePattern {
         }
 
         // Create new waves based on scaled time.
-        double scaledDelta = deltaMs * TIME_SCALE;
+        double scaledDelta = deltaMs * timeScaleParam.getValue();
         waveTimer += scaledDelta / 1000;
         if(waveTimer > nextWaveTime){
             waveTimer = 0;
-            nextWaveTime = randomDouble(random, 20, 30, 100);
+            double minWaveTime = minWaveTimeParam.getValue();
+            double maxWaveTime = Math.max(minWaveTime, maxWaveTimeParam.getValue());
+            nextWaveTime = randomDouble(random, minWaveTime, maxWaveTime, 1000);
 
-            double minDirection = lastWaveDirection + MIN_WAVE_DIRECTION_OFFSET;
-            double maxDirection = lastWaveDirection - MIN_WAVE_DIRECTION_OFFSET + TAU;
-            double direction = randomDouble(random, minDirection, maxDirection, 360);
+            // To get a more balanced distribution of wave directions, the next wave direction is offset from the
+            // the previous wave direction.
+            double offset = offsetParam.getValue();
+            double minDirection = lastWaveDirection + offset;
+            double maxDirection = lastWaveDirection - offset + TAU;
+            double direction = randomDouble(random, minDirection, maxDirection, 1000);
+            direction = clampAngle(direction);
             lastWaveDirection = direction;
 
-            double size = randomDouble(random, 1, 3.5, 100);
-            waves.add(new Wave(MIN_POSITION, SPEED, size, direction, 100));
+            double size = randomDouble(random, minWaveAngleParam.getValue(), maxWaveAngleParam.getValue(), 1000);
+            double speed = waveSpeedParam.getValue();
+            double intensity = waveBrightnessParam.getValue();
 
-            //waves.add(new Wave(MIN_POSITION, SPEED, 1, 0, 100));
+            waves.add(new Wave(direction, size, speed, intensity));
 
             System.out.println("\nnew wave: " + direction + " " + size);
-            System.out.println("next wave: " + nextWaveTime);
+            System.out.println("next wave time: " + nextWaveTime);
         }
 
-        // Turn everything off.
+        // Set every pixel to base brightness.
         for (LXPoint p : model.points) {
-            //colors[p.index] = LXColor.BLACK;
-            colors[p.index] = LXColor.hsb(0, 0, MIN_BRIGHTNESS);
+            colors[p.index] = LXColor.hsb(0, 0, baseBrightnessParam.getValue());
         }
 
         // Animate the waves.
@@ -101,76 +127,84 @@ public class WavePattern extends TemplePattern {
         }
     }
 
-    private static double MIN_POSITION = 1000; // Minimum measured radius: 3508
-    private static final double MAX_POSITION = 100000;  // Maximum measured radius: 28769
-
     private class Wave {
 
-        double direction; // Direction of the center of the wave, in radians.
-        double minAngle;
-        double maxAngle;
-        double speed;     // Rate that wave's arc radius expands.
-        double position;  // Current arc radius.
+        double minAngle;  // Minimum wave angle. Equal to direction - size/2.
+        double maxAngle;  // Maximum wave angle. Equal to direction + size/2.
+        double speed;     // Rate that the wave's arc radius expands.
         double intensity; // Maximum brightness at the leading edge of the wave.
+        double position = MIN_POSITION;  // Current arc radius.
 
-        public Wave(double position, double speed, double size, double direction, double intensity) {
-            this.position = position;
-            this.speed = speed;
+        public Wave(double direction, double size, double speed, double intensity) {
             this.minAngle = clampAngle(direction - size/2);
             this.maxAngle = clampAngle(direction + size/2);
-            this.direction = direction;
+            this.speed = speed;
             this.intensity = intensity;
         }
 
         public void run(double deltaMs) {
+
+            double fadeDistance = fadeDistanceParam.getValue();
+            double fadeAngle = fadeAngleParam.getValue();
+            double decay = decayParam.getValue();
+            double baseBrightness = baseBrightnessParam.getValue();
+
             position += speed * deltaMs / 1000;
 
             for (int i=0; i<numPoints; i++){
                 double angle = angles[i];
                 if (!isAngleInRange(angle, minAngle, maxAngle)){
+                    // Pixel is outside of wave angle. Skip it.
                     continue;
                 }
                 double radius = radii[i];
                 if (radius > position) {
+                    // Pixel is outside of wave radius. Skip it.
                     continue;
                 }
                 double oldBrightness = LXColor.b(colors[model.points[i].index]);
                 double brightness;
-                if (radius > position - FRONT_FADE) {
-                    // handle fade into wave
-                    brightness = (int)linearInterpolate(position - radius, 0, oldBrightness, FRONT_FADE, intensity);
+                if (radius > position - fadeDistance) {
+                    // Pixel is between peak and fade distance. Fade it in with linear interpolation.
+                    brightness = (int)linearInterpolate(position - radius, 0, oldBrightness, fadeDistance, intensity);
+                    brightness = Math.min(brightness, 100);
 
                 } else {
-                    brightness = (int) (intensity * Math.pow(2, (radius - position + FRONT_FADE) / DECAY));
-                    brightness = Math.max(brightness, MIN_BRIGHTNESS);
+                    // Pixel is within the wave peak. Brightness decays exponentially with distance from peak.
+                    brightness = (int) (intensity * Math.pow(2, (radius - position + fadeDistance) / decay));
+                    brightness = Math.max(brightness, baseBrightness);
+                    brightness = Math.min(brightness, 100);
+                    // TODO: maybe exponential decay should instead be offset by base brightness.
                 }
-                // handle side fade
-                if (isAngleInRange(angle, maxAngle - SIDE_FADE, maxAngle)){
-                    brightness = (int)linearInterpolate(maxAngle - angle, 0, oldBrightness, SIDE_FADE, brightness);
+                if (isAngleInRange(angle, maxAngle - fadeAngle, maxAngle)){
+                    // Pixel is within upper fade angle. Fade it in.
+                    brightness = (int)linearInterpolate(maxAngle - angle, 0, oldBrightness, fadeAngle, brightness);
                 }
-                if (isAngleInRange(angle, minAngle, minAngle + SIDE_FADE)){
-                    brightness = (int)linearInterpolate(angle - minAngle, 0, oldBrightness, SIDE_FADE, brightness);
+                if (isAngleInRange(angle, minAngle, minAngle + fadeAngle)){
+                    // Pixel is within lower fade angle. Fade it in.
+                    brightness = (int)linearInterpolate(angle - minAngle, 0, oldBrightness, fadeAngle, brightness);
                 }
                 colors[model.points[i].index] = LXColor.hsb(0, 0, brightness);
             }
         }
 
+        // Returns whether to remove the wave from the simulation.
         public boolean done() {
+            // TODO: it might be better to compute whether the dimmest pixel is within some tolerance of the base brightness, but this seems safer for now.
             return position > MAX_POSITION;
         }
     }
 
-    // Returns whether an angle is within a range, while accounting for the boundary. All values are expected to be
-    // in range [0, 2PI).
+    // Returns whether an angle is within a range. Values are expected to be in range [0, 2PI).
     private static boolean isAngleInRange(double angle, double min, double max){
         if (min < max) {
             return angle >= min && angle <= max;
         }
-        // else the range wraps around the circle boundary.
+        // The range wraps around the circle boundary.
         return angle >= min || angle < max;
     }
 
-    // Limits an angle in radians to range [0, 2PI).
+    // Limits an angle to range [0, 2PI).
     private static double clampAngle(double angle) {
         while(angle < 0){
             angle += TAU;
@@ -181,14 +215,17 @@ public class WavePattern extends TemplePattern {
         return angle;
     }
 
+    // Returns the angle from a point to the origin;
     private static double angle(LXPoint point) {
         return Math.atan2(point.z, point.x) + Math.PI;
     }
 
+    // Returns a random double between min and max with specified steps of precision.
     private static double randomDouble(Random r, double min, double max, int precision){
         return r.nextInt(precision) * (max - min) / precision + min;
     }
 
+    // Returns a linearly interpolated y value given an x value and two points on a line.
     private static double linearInterpolate(double x, double x0, double y0, double x1, double y1){
         return (y0*(x1-x)+y1*(x-x0))/(x1-x0);
     }
