e018c6a37c5f24cdd195625b3684523bbc13f5a7
Gerald Spencer <gerald@3scan.com>
2018-08-04 22:27:46 -0700

    Petal from lower and upper

--- src/main/java/org/templegalaxia/model/Petal.java
+++ src/main/java/org/templegalaxia/model/Petal.java
@@ -15,19 +15,32 @@ public class Petal extends LXModel {
     super(new Fixture(transform));
   }
 
+  public Petal(LowerPetal lowerPetal, UpperPetal upperPetal){
+    super(new Fixture(lowerPetal, upperPetal));
+  }
+
+  private static class LoadPetalChunks {
+    LoadPetalChunks(Fixture fixture, LowerPetal lowerPetal, UpperPetal upperPetal){
+      numPixels = lowerPetal.getPoints().size() + upperPetal.getPoints().size();
+      for (int lowerItr = LowerPetal.numPixels - 1; lowerItr >= 0; lowerItr--){
+        fixture.addPoint(lowerPetal.getPoints().get(lowerItr));
+      }
+
+      for (LXPoint p : upperPetal.getPoints()){
+        fixture.addPoint(p);
+      }
+    }
+  }
+
   private static class Fixture extends LXAbstractFixture {
+    Fixture(LowerPetal lowerPetal, UpperPetal upperPetal){
+      new LoadPetalChunks(this, lowerPetal, upperPetal);
+    }
     Fixture(LXTransform transform) {
       LowerPetal lowerPetal = new LowerPetal(transform);
       UpperPetal upperPetal = new UpperPetal(transform);
 
-      numPixels = lowerPetal.getPoints().size() + upperPetal.getPoints().size();
-      for (int lowerItr = LowerPetal.numPixels - 1; lowerItr >= 0; lowerItr--){
-        addPoint(lowerPetal.getPoints().get(lowerItr));
-      }
-
-      for (LXPoint p : upperPetal.getPoints()){
-        addPoint(p);
-      }
+      new LoadPetalChunks(this, lowerPetal, upperPetal);
     }
   }
 }
--- src/main/java/org/templegalaxia/model/Temple.java
+++ src/main/java/org/templegalaxia/model/Temple.java
@@ -5,7 +5,6 @@ import heronarts.lx.model.LXModel;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.transform.LXTransform;
 import java.util.*;
-import org.templegalaxia.patterns.TemplePattern;
 import processing.core.PApplet;
 
 public class Temple extends LXModel {
@@ -14,7 +13,6 @@ public class Temple extends LXModel {
 
   public final List<Petal> petals;
   public final List<Ring> rings;
-  private final Map<LXPoint, Integer> pointToPixelMap;
 
   public Temple(PApplet applet) {
     super(new Fixture(applet));
@@ -23,35 +21,26 @@ public class Temple extends LXModel {
 
     this.petals = Collections.unmodifiableList(f.petals);
     this.rings = Collections.unmodifiableList(f.rings);
-    this.pointToPixelMap = Collections.unmodifiableMap(f.pointToPixelMap);
-  }
-
-  /**
-   * Return the global pixel index of the given model point
-   *
-   * @param point the point in question
-   * @return The global pixel index
-   */
-  public int getPixelIndex(LXPoint point) {
-    return this.pointToPixelMap.get(point);
   }
 
   private static class Fixture extends LXAbstractFixture {
     private final List<Petal> petals = new ArrayList<>();
     private final List<Ring> rings = new ArrayList<>();
-    private final Map<LXPoint, Integer> pointToPixelMap = new HashMap<>();
 
     Fixture(PApplet applet) {
       LXTransform transform = new LXTransform();
-      int pixelsPerPetal = -1;
 
       // Build the spokes
       for (int i = 0; i < NUMBER_OF_PETALS; ++i) {
-        Petal petal = new Petal(transform);
+        LowerPetal lowerPetal = new LowerPetal(transform);
+        UpperPetal upperPetal = new UpperPetal(transform);
+
+        Petal petal = new Petal(lowerPetal, upperPetal);
         addPoints(petal);
+
         this.petals.add(petal);
+
         transform.rotateY(ANGLE_BETWEEN_PETALS);
-        if (pixelsPerPetal == -1) pixelsPerPetal = petal.size;
       }
 
       for (int pointItr = 0; pointItr < Petal.numPixels; ++pointItr) {
@@ -60,8 +49,6 @@ public class Temple extends LXModel {
         for (int petalItr = 0; petalItr < NUMBER_OF_PETALS; ++petalItr) {
           LXPoint point = petals.get(petalItr).getPoints().get(pointItr);
           points.add(point);
-          int pixelIdx = TemplePattern.petalIndexToPointIndex(pointItr, petalItr);
-          pointToPixelMap.put(point, pixelIdx);
         }
         rings.add(new Ring(points));
       }
