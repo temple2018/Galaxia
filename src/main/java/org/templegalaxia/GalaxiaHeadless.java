19395855d940462f3837c128d5ac14543c913272
meawoppl <meawoppl@gmail.com>
2018-08-14 17:41:57 -0700

    Spotless

--- src/main/java/org/templegalaxia/GalaxiaHeadless.java
+++ src/main/java/org/templegalaxia/GalaxiaHeadless.java
@@ -3,7 +3,6 @@ package org.templegalaxia;
 import heronarts.lx.LX;
 import heronarts.lx.LXLoopTask;
 import heronarts.lx.model.LXModel;
-import heronarts.lx.studio.LXStudio;
 import org.templegalaxia.configuration.Outputs;
 import org.templegalaxia.model.Temple;
 
@@ -27,20 +26,20 @@ public class GalaxiaHeadless {
     lx = new LX(model);
     Outputs outputs = new Outputs(lx, model);
 
-    lx.engine.addLoopTask(new LXLoopTask() {
-      @Override
-      public void loop(double v) {
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - lastHeartBeat > 1000.0) {
-          lastHeartBeat = currentTime;
-          System.out.println(lx.engine.getDefaultChannel().getOscAddress());
-        }
-      }
-    });
+    lx.engine.addLoopTask(
+        new LXLoopTask() {
+          @Override
+          public void loop(double v) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastHeartBeat > 1000.0) {
+              lastHeartBeat = currentTime;
+              System.out.println(lx.engine.getDefaultChannel().getOscAddress());
+            }
+          }
+        });
 
     // Kick it!
     System.out.println("Starting engine!");
     lx.engine.start();
   }
-
 }
