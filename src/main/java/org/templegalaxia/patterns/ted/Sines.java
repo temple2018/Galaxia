7dd4913451b1d0a49a013ecb3f07b5e8e4405ab3
Mark Slee <mcslee@gmail.com>
2018-08-21 17:32:08 -0700

    Add modulation and clean up brightness in Sines

--- src/main/java/org/templegalaxia/patterns/ted/Sines.java
+++ src/main/java/org/templegalaxia/patterns/ted/Sines.java
@@ -7,21 +7,22 @@ import heronarts.lx.model.LXPoint;
 import heronarts.lx.modulator.LXModulator;
 import heronarts.lx.modulator.SawLFO;
 import heronarts.lx.parameter.BoundedParameter;
+import heronarts.lx.parameter.CompoundParameter;
 import org.templegalaxia.model.Ring;
 import org.templegalaxia.patterns.TemplePattern;
 
 @LXCategory("Tedward")
 public class Sines extends TemplePattern {
-  BoundedParameter speedParam =
-      new BoundedParameter("speed", 45000, 90000, 1000)
+  CompoundParameter speedParam =
+      new CompoundParameter("speed", 45000, 90000, 1000)
           .setDescription("Period of the base oscillator.");
-  BoundedParameter freqParam =
-      new BoundedParameter("frequency", 2, 0.1, 10).setDescription("Number of cycles per ring.");
-  BoundedParameter phaseOffsetParam =
-      new BoundedParameter("phaseOffset", 0.12, 0, Math.PI / 2)
+  CompoundParameter freqParam =
+      new CompoundParameter("frequency", 2, 0.1, 10).setDescription("Number of cycles per ring.");
+  CompoundParameter phaseOffsetParam =
+      new CompoundParameter("phaseOffset", 0.12, 0, Math.PI / 2)
           .setDescription("Phase offset between adjacent rings.");
-  BoundedParameter brightnessParam =
-      new BoundedParameter("brightness", 0.5, 0, 1).setDescription("Global scaling factor.");
+  CompoundParameter brightnessParam =
+      new CompoundParameter("brightness", 0.5, 0, 1).setDescription("Global scaling factor.");
   LXModulator time = new SawLFO(0, 2 * Math.PI, speedParam);
 
   public Sines(LX lx) {
@@ -36,15 +37,16 @@ public class Sines extends TemplePattern {
   @Override
   protected void run(double v) {
     double phase = 0;
+    double bright = brightnessParam.getValue();
     for (int i = 0; i < model.rings.size(); i++) {
       Ring ring = model.rings.get(i);
       int j = 0;
       for (LXPoint point : ring.getPoints()) {
         double angle = 2 * Math.PI / ring.getPoints().size() * j;
-        double value = Math.sin(time.getValue() + freqParam.getValue() * angle + phase) + 0.5;
+        double value = Math.sin(time.getValue() + freqParam.getValue() * angle + phase);
         value = 100 * Math.abs(value); // normalize
-        value *= brightnessParam.getValue(); // attenuate
-        colors[point.index] = LXColor.hsb(0, 0, value);
+        value *= bright; // attenuate
+        colors[point.index] = LXColor.gray(value);
         j++;
       }
       phase += phaseOffsetParam.getValue();
